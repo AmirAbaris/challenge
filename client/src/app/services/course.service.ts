c07e3e54a30aeb63b53ec78cf024467f245d7f32
Abaris <118467205+AmirAbaris@users.noreply.github.com>
2023-12-26 19:40:16 +0330

    added service

---
+++ client/src/app/services/course.service.ts
@@ -0,0 +1,22 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable, inject } from '@angular/core';
+import { Observable, map } from 'rxjs';
+import { Course } from '../models/course.model';
+import { environment } from '../../environments/environment.development';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CourseService {
+  readonly #http = inject(HttpClient);
+
+  readonly #baseApiUrl = environment.apiUrl + 'course/';
+
+  getAll(): Observable<Course | null> {
+    return this.#http.get<Course>(this.#baseApiUrl + 'get-all').pipe(
+      map((res: Course | null) => {
+        return res || null;
+      })
+    );
+  }
+}
