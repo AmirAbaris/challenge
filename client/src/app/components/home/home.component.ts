6077d6e0feea81c6bf7a0dca213e93e2510a93e1
Abaris <118467205+AmirAbaris@users.noreply.github.com>
2023-12-26 20:56:11 +0330

    fixed minor bugs

--- client/src/app/components/home/home.component.ts
+++ client/src/app/components/home/home.component.ts
@@ -3,11 +3,16 @@ import { Component, DestroyRef, OnInit, inject } from '@angular/core';
 import { CourseService } from '../../services/course.service';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { Course } from '../../models/course.model';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatIconModule } from '@angular/material/icon';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatCardModule, MatIconModule, MatProgressSpinnerModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
@@ -25,7 +30,8 @@ export class HomeComponent implements OnInit {
     this.#courseService.getAll().pipe(takeUntilDestroyed(this.#destroyRef)).subscribe({
       next: (res) => {
         this.courses = res;
-      }
+      },
+      error: (err) => console.log(err)
     });
   }
 }
