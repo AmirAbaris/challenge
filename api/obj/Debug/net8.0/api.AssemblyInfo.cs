7d9e5db956a29734457b74fb1304d64e84aca5a6
Abaris <118467205+AmirAbaris@users.noreply.github.com>
2023-12-26 19:55:57 +0330

    clean up

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c07e3e54a30aeb63b53ec78cf024467f245d7f32")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a431b4ad1d3e49a7156054fffef2e7deb1923df3")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
