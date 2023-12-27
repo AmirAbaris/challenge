3a72df7caf587fae3eb10c4bc24d3dcf1c765124
Abaris <118467205+AmirAbaris@users.noreply.github.com>
2023-12-27 16:59:58 +0330

    fixed routes

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,11 +1,12 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { CourseComponent } from './components/course/course.component';
+import { CourseDetailComponent } from './components/course-detail/course-detail.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
     { path: 'home', component: HomeComponent },
     { path: 'course', component: CourseComponent },
-    { path: 'course-detail/:id', component: CourseComponent },
+    { path: 'course/:id', component: CourseDetailComponent },
     { path: '**', redirectTo: '' }
 ];
